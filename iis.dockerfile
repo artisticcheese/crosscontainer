2a4d27ed44898473b4048f09512ceb8ce85cb67a
gregory <gregory@ad.local>
2018-01-27 16:26:05 -0800

    Removed SQL related routines

--- iis.dockerfile
+++ iis.dockerfile
@@ -2,8 +2,6 @@
 FROM microsoft/iis
 SHELL ["powershell", "-Command", "$ErrorActionPreference = 'Stop'; $ProgressPreference = 'Continue'; $verbosePreference='Continue';"]
 WORKDIR prep
-RUN Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force
-RUN Find-module sqlserver | install-module -force
 RUN install-windowsfeature Web-Asp-Net45
 COPY sql.aspx c:/inetpub/wwwroot/
 COPY web.config c:/inetpub/wwwroot/
--- run.ps1
+++ run.ps1
@@ -4,8 +4,3 @@ docker build -t artisticcheese/crosscontainersql -f sql.dockerfile .
 
 docker run -d --rm -p 80:80 -h containerhost --name iis --security-opt "credentialspec=file://win.json" artisticcheese/crosscontaineriis
 docker run -d --rm -h containerhost --name sql -e sa_password=A123456! -e ACCEPT_EULA=Y --security-opt "credentialspec=file://win.json" artisticcheese/crosscontainersql
-
-
-#docker exec iis powershell.exe -command "(invoke-sqlcmd 'Select @@Version' -ConnectionString 'data source=sql;Integrated Security=True')"
-
-#docker exec iis powershell.exe -command "(invoke-sqlcmd 'Select system_user' -ConnectionString 'data source=sql;Integrated Security=True')"
